b6794d8faefc66c19fd464c02c52a54c73a05589
Paul Dziemiela <Paul.Dziemiela@erg.com>
2022-11-06 18:53:12 -0500

    tidy up non-functional in windows name tag

--- config-compose.ps1
+++ config-compose.ps1
@@ -1,6 +1,6 @@
 
 $env:CURRENT_UID='1000:1000' 
-$env:COMPOSE_PROJECT_NAME="config-compose"
+$env:COMPOSE_PROJECT_NAME="cip20-config-compose"
 $env:COMPOSE_PROFILE=$args[0]
 $env:PROJDIR=$args[1]
 
--- config-compose.sh
+++ config-compose.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-CURRENT_UID=$(id -u):$(id -g) COMPOSE_PROJECT_NAME=config-compose COMPOSE_PROFILE=${1} PROJDIR=${2} docker-compose -f ./config-compose.yml up
+CURRENT_UID=$(id -u):$(id -g) COMPOSE_PROJECT_NAME=cip20-config-compose COMPOSE_PROFILE=${1} PROJDIR=${2} docker-compose -f ./config-compose.yml up
