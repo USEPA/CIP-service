ae1a3582d30a2aeea7cb084c91acab5c47487e37
Paul Dziemiela <Paul.Dziemiela@erg.com>
2022-12-13 19:29:55 -0500

    cleanup for linux

--- config/config-compose.sh
+++ config/config-compose.sh
@@ -4,7 +4,7 @@ SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 
 CURRENT_UID=$(id -u):$(id -g)                \
-COMPOSE_PROJECT_NAME=cipsrv-config-compose   \
+COMPOSE_PROJECT_NAME=config                  \
 SCRIPT_PATH=${SCRIPTPATH}                    \
 BUNDLE=${1}                                  \
 BPROFILE=${2}                                \
