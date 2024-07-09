980c19f1aef2739f8d48b373254d33f46b1c9349
Paul Dziemiela <paul@dziemiela.com>
2024-07-09 15:12:54 -0400

    bump

--- config/config-compose.py
+++ config/config-compose.py
@@ -85,10 +85,10 @@ for file in os.listdir(templates):
          with open(os.path.join(outdir,output),"w") as file:
             file.write(outrender);
 
-   if file == '.env':
+   if os.path.basename(file) == '.env':
       has_env = True;
       
-   if file == 'sample.env':
+   if os.path.basename(file) == 'sample.env':
       has_sample_env = True;
 
 if has_env is True:
