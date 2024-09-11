506539196e2fe1a9c4ba9641261e526afa27d066
Paul Dziemiela <paul@dziemiela.com>
2024-09-11 06:53:12 -0400

    add pgtap to support test coverage

--- engine/postgresql/db_config.sql
+++ engine/postgresql/db_config.sql
@@ -2,5 +2,6 @@ CREATE EXTENSION IF NOT EXISTS hstore;
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE EXTENSION IF NOT EXISTS postgis;
 CREATE EXTENSION IF NOT EXISTS pgrouting;
+CREATE EXTENSION IF NOT EXISTS pgtap;
 
 GRANT ALL ON TABLE public.spatial_ref_sys TO cipsrv,cipsrv_user,cipsrv_upload;
