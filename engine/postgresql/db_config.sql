ae23fc3fd33e9e5bfeae836cdef17083f06406c3
Paul Dziemiela <paul@dziemiela.com>
2024-09-14 10:14:56 -0400

    Add basic support for profiling

--- engine/postgresql/db_config.sql
+++ engine/postgresql/db_config.sql
@@ -3,5 +3,6 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE EXTENSION IF NOT EXISTS postgis;
 CREATE EXTENSION IF NOT EXISTS pgrouting;
 CREATE EXTENSION IF NOT EXISTS pgtap;
+CREATE EXTENSION IF NOT EXISTS plprofiler;
 
 GRANT ALL ON TABLE public.spatial_ref_sys TO cipsrv,cipsrv_user,cipsrv_upload;
